7888f2e7f478c29b68ea5ef719047d31182b0a49
jeffkit <jeff@fallever.com>
2011-05-24 16:42:41 +0800

    user xmlHandler for default result handler

--- application.py
+++ application.py
@@ -1,12 +1,8 @@
 #encoding=utf-8
 import urllib,urllib2
 from monitor import Monitor
-
+from resultHandler import xmlHandler
 if __name__ == '__main__':
-    # 演示结果处理器可以配置
-    class Handler:
-        pass
-    rh = Handler()
-    setattr(rh,'handle',lambda x:log.debug('handle result in your way!'))
+    rh = xmlHandler()
     Monitor(result_handler=rh).run()
 
